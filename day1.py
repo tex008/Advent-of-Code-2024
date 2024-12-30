33a2278d87e1f2abebef33f2586561ec008f4e98
tex008 <texera009@gmail.com>
2024-12-30 20:24:09 -0300

    finish the `day1-part2` of `Advent of Code 2024`

--- day1.py
+++ day1.py
@@ -1018,7 +1018,23 @@ def main():
         )
     )
 
-    print(lists_total_difference)
+    similarity_occurrences = {}
+    for location_id in first_list:
+        if location_id in second_list:
+            similarity_occurrences[location_id] = (
+                similarity_occurrences.get(location_id, 0) + 1
+            )
+
+        else:
+            similarity_occurrences[location_id] = 1
+
+    similarity_score = sum(
+        location_id * similarity_occurrences.get(location_id, 0)
+        for location_id in similarity_occurrences
+    )
+
+    print(f"lists_total_difference: {lists_total_difference}")
+    print(f"similarity_score: {similarity_score}")
 
 
 main()
